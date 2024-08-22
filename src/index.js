f3eae9aabe9af3bd3374f670a3606960f29859bc
Abhishek <mehtaabhishek499@gmail.com>
2024-08-22 18:51:44 +0530

    Project File and Database Connection

---
+++ src/index.js
@@ -0,0 +1,53 @@
+// require('dotenv').config({path:'./env'})
+
+// import mongoose from "mongoose"
+// import { DB_NAME } from "./constants"
+
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js"
+
+dotenv.config({ path: './env' })
+
+
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//      ------ IST APPRROACH TO CONNECT DATABASE-------
+// (async()=>{
+//         try {
+//             await mongoose.connect(`${process.env.MONGODB_URI} / ${DB_NAME}`)
+//         } catch (error) {
+//             console.error("Error: ",error)
+//         }
+// })()
+
+
+// (async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI} / ${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.error("Error connecting to MongoDB: ", error)
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error("Error: ",error)
+//     }
+// })()
